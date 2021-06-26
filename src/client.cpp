ae82708c4ea7be4800885629debe700aeabc9d5a
k-al <kai_a@t-online.de>
2021-06-26 23:05:42 +0200

    client got around 5 lines

--- src/client.cpp
+++ src/client.cpp
@@ -1,4 +1,17 @@
 
+#include <SFML/Graphics.hpp> // included by .hpp
+
 
 #include "client.hpp"
 
+Client::~Client () {
+    // cleanup
+}
+
+Client::run () {
+    this->window.create(sf::VideoMode::getFullscreenModes()[0], "Tarnish");
+    
+    while (this->window.isOpen()) {
+        
+    }
+}
--- src/client.hpp
+++ src/client.hpp
@@ -1,5 +1,5 @@
 
-
+#include <SFML/Graphics.hpp>
 
 
 class Client {
@@ -7,7 +7,20 @@ class Client {
     Server* servre = nullptr;
     Thing* follow = nullptr;
     
+    sf::Window main_window;
+    
+    sf::View main_view;
     
     
+    Client () {}
     
-}
+    ~Client ();
+    
+    
+    // creates a Window and manage it
+    // the calling thread should be the main thread (iOS compatibility) and only returns if the Window is closed
+    void run ();
+    
+    void change_follow (Thing*);
+    
+};
