80f117c0d5ab716cd11ab0e6cc41132f4586a1c6
k-al <kai_a@t-online.de>
2021-09-25 13:54:19 +0200

    little change at visible.hpp to get used to git agian

--- src/objects/visible.hpp
+++ src/objects/visible.hpp
@@ -1,7 +1,23 @@
 
+#include "ivec.hpp"
 #include "objects/thing.hpp"
 
 class Visible : public Thing {
   public:
     sf::Sprite sprite;
+    
+    //# These are static informations for a type of objects. This not have to stored multiple times.
+    std::string texture;
+    std::vector<uint64_t> animations; // start and endpoits of the frames for all animations
+    
+    uint64_t last_tick; // Last game_tick that this object has been updatet. For timelapsing.
+    
+    uint64_t current_frame; // current frame of animation
+    uint64_t end_frame; // end point of current animation
+    
+    sf::Sprite* update (uint64_t game_tick, iVec2 active_chunk, iVec2 center_position);
+    bool load_texture ();
+    void unload_texture ();
+    
+    
 };
